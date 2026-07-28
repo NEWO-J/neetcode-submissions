249b426bb2ead53576846e2e5ac3891f73a99c03
Jonah Owen <61209323+NEWO-J@users.noreply.github.com>
2026-07-27 19:56:31 -0500

    Add: time-based-key-value-store - submission-8

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-8.py
@@ -0,0 +1,39 @@
+class TimeMap:
+
+    def __init__(self):
+        self.timelist = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.timelist:
+            self.timelist[key] = [[timestamp, value]]
+        else:
+            self.timelist[key].append([timestamp, value])
+        print(self.timelist)
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key in self.timelist:
+            timelist = self.timelist[key]
+        else:
+            return ""
+        l = 0
+        r = len(timelist) - 1
+
+        while l < r:
+            midpoint = (l + r) // 2
+
+            if timestamp > timelist[midpoint][0]:
+                l = midpoint + 1
+            else:
+                r = midpoint  
+            
+        # we dont want to look at timestamps for irrelevant key
+
+        if timelist[l][0] > timestamp:
+            if timelist[l - 1][0] < timestamp:
+                return timelist[l - 1][1]
+
+            return ""
+        return timelist[l][1]
+
+

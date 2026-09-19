cada9327e0c1a78f77f53f3d1ebc1e9e9aaf4744
Jonah Owen <61209323+NEWO-J@users.noreply.github.com>
2026-09-19 17:55:17 -0500

    Add: subsets - submission-4

---
+++ Data Structures & Algorithms/subsets/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+            result = []
+            built = []
+            def sub(built, index):
+                nonlocal nums
+                nonlocal result
+                if index == len(nums):
+                    result.append(built[:])
+                    return
+
+                sub(built, index + 1)
+
+                built.append(nums[index])
+                sub(built, index + 1)
+                built.pop()
+            
+            sub(built, 0)
+            return result
+    
